f29b4e5f9eaa460fc5e9056362bb64e4c1bf4705
VladimirZaigraev <v.a.zaigraev@mail.ru>
2021-03-09 22:30:56 +0300

    fixt: connect yandex to main.js

--- js/main.js
+++ js/main.js
@@ -13,3 +13,13 @@ const swiper = new Swiper(".swiper-container", {
     onlyInViewport: false,
   },
 });
+//подключение карты
+type = "text/javascript";
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [12.93434771, 100.88389939],
+
+    zoom: 16,
+  });
+}
